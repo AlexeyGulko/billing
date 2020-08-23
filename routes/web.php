8f9b2791217cbf925603c5e1165bbedcf6b2ce59
Alexey Gulko <dev.gulkoalexey@gmail.com>
2020-08-23 19:31:07 +0300

    change routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'PaymentController@create')->name('home');
 
 Route::get('/payments', 'PaymentController@index')->name('payments.index');
 Route::get('/payments/create', 'PaymentController@create')->name('payments.create');
